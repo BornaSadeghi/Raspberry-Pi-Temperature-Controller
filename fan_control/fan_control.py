fc0e30322a41b5145ba395f172e755896660b99c
BornaSadeghi <sadeghiborna@gmail.com>
2020-11-29 20:18:15 -0500

    More modular, x-axis shifts

--- fan_control/fan_control.py
+++ fan_control/fan_control.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
 from gpiozero import DigitalOutputDevice, CPUTemperature
 from matplotlib import pyplot as plt, style
-from matplotlib.animation import FuncAnimation
 from time import sleep, time
 
 fan = DigitalOutputDevice(17)
@@ -9,58 +11,77 @@ cpu = CPUTemperature()
 fan_on = False
 
 TEMPERATURE_THRESHOLD = 68
-# Waiting time before turning fan off when temperature is low enough
+# Time between temperature checks when fan is off
 FAN_OFF_DELAY = 10
-# Time between temperature checks
-UPDATE_DELAY = 5
-
-assert(FAN_OFF_DELAY >= UPDATE_DELAY)
+# Time between temperature checks when fan is on
+FAN_ON_DELAY = 5
 
 # Plot temperature over time
 x_data = [0]
 y_data = [cpu.temperature]
-
-def fan_on():
-    if fan.value == 0:
-        print("Fan is on")
-    fan.on()
-
-def fan_off():
-    sleep(FAN_OFF_DELAY - UPDATE_DELAY)
-    if fan.value == 1:
-        print("Fan is off")
-    fan.off()
-
-style.use("fivethirtyeight")
-figure = plt.figure()
-ax1 = figure.add_subplot(1,1,1)
-
-# How much higher/lower the y-axis should show compared to max/min value
-Y_PADDING = 5
-
 start_time = time()
 lowest_temperature = 1000
 highest_temperature = 0
+
+# How much higher/lower the y-axis should show compared to max/min value
+Y_PADDING = 5
+MAX_ENTRIES = 10
+
+style.use("fivethirtyeight")
+figure = plt.figure()
+ax = figure.add_subplot(1,1,1)
 
 plt.title("CPU Temperature (°C) over Time (s)")
 plt.ylabel("CPU Temperature (°C)")
 plt.xlabel("Time (s)")
 plt.tight_layout()
+plt.ion()
+plt.show()
 
-def update(i):
+def fan_on():
+    if fan.value == 0:
+        print("Fan is on")
+        fan.on()
+    sleep(FAN_ON_DELAY)
+
+def fan_off():
+    if fan.value == 1:
+        print("Fan is off")
+        fan.off()
+    sleep(FAN_OFF_DELAY)
+
+def update_data():
     global lowest_temperature, highest_temperature
     x_data.append(time()-start_time)
     y_data.append(cpu.temperature)
-    print("Time: {} s, CPU Temperature: {} °C".format(x_data[-1], cpu.temperature))
-    
+
+    if len(x_data) > MAX_ENTRIES:
+        del x_data[0]
+        del y_data[0]
+
     lowest_temperature = min(cpu.temperature, lowest_temperature)
     highest_temperature = max(cpu.temperature, highest_temperature)
+    print("Time: {} s, CPU Temperature: {} °C".format(x_data[-1], cpu.temperature))
+
+def update_chart():
+    ax.clear()
+
+    plt.title("CPU Temperature (°C) over Time (s)")
+    plt.ylabel("CPU Temperature (°C)")
+    plt.xlabel("Time (s)")
+    plt.tight_layout()
     
     plt.ylim(lowest_temperature-Y_PADDING, highest_temperature+Y_PADDING)
-    ax1.plot(x_data, y_data, 'r')
+    ax.plot(x_data, y_data, 'r')
+    figure.canvas.draw()
+    plt.pause(0.01)
     
-    fan_on() if cpu.temperature > TEMPERATURE_THRESHOLD else fan_off()
+    
 
-anim = FuncAnimation(figure, update, interval=UPDATE_DELAY*1000)
-plt.show()
-
+while True:
+    try:
+        update_data()
+        update_chart()
+        fan_on() if cpu.temperature > TEMPERATURE_THRESHOLD else fan_off()
+    except KeyboardInterrupt:
+        break
